6d62ccbbc66bb9f805900f36452589d0d1ca625a
Javi <fjruiz.1992@gmail.com>
2024-04-24 15:30:04 +0200

    Gokapi via https and remove raspibot

--- raspibot-compose/RaspiBot/raspibot.py
+++ raspibot-compose/RaspiBot/raspibot.py
@@ -5,15 +5,6 @@ import os
 BOT_ID = os.environ['BOT_ID'].replace('"','')
 bot = telebot.TeleBot(BOT_ID)
 
-@bot.message_handler(commands=['vention'])
-def venti_on(message):
-    bot.reply_to(message, vention())
-
-@bot.message_handler(commands=['ventioff'])
-def venti_off(message):
-    bot.reply_to(message, ventioff())
-
-
 @bot.message_handler(commands=['temperatura'])
 def send_temp(message):
     bot.reply_to(message, get_temp())
@@ -27,6 +18,9 @@ def ip(message):
 def disco(message):
     bot.reply_to(message, get_disco())
 
+@bot.message_handler(commands=['grabacion'])
+def grabacion(message):
+    bot.reply_to(message, get_rec())
 
 def get_ip():
     process = subprocess.Popen(['/usr/bin/curl', 'ifconfig.me'],
@@ -45,7 +39,7 @@ def get_temp():
 
 
 def get_disco():  
-    cmd = "df -h | grep 'Filesystem\|overlay\|/dev/sda1\|/dev/sdb1'"
+    cmd = "df -h | grep 'Filesystem\|/dev/mmcblk0p2\|/dev/sda1\|/dev/nvme0n1'"
     process = subprocess.Popen([cmd],
         shell=True,
         stdout=subprocess.PIPE,
@@ -53,24 +47,14 @@ def get_disco():
     stdout, stderr = process.communicate()
     return stdout
 
-def vention():
-    ventilador = "/home/pi/ventilador"
-    cmd = "ssh pi@retropie 'touch {}'".format(ventilador)    
+def get_rec():  
+    cmd = "ls -R /dev/sdb1/recourdbate | grep part"
     process = subprocess.Popen([cmd],
         shell=True,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
-    return "Ventilador encendido"
+    return stdout
 
-def ventioff():
-    ventilador = "/home/pi/ventilador"
-    cmd = "ssh pi@retropie 'rm {}'".format(ventilador)    
-    process = subprocess.Popen([cmd],
-        shell=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
-    return "Ventilador apagado"
 
 bot.polling()
